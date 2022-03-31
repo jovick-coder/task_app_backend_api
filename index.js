19d0dc9fb401afe57b59d6a8a44fcf8c8a5c6b73
Jovick-coder <victorjosiahm3@gmail.com>
2022-03-31 08:47:45 +0000

    cors v2

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use(
     "Access-Control-Allow-Origin": "*",
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.use(express.json());
 
