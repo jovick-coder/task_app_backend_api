eb2a6ea9eca36ad32ff2af9bffa0e427ee1404ca
Jovick-coder <victorjosiahm3@gmail.com>
2022-03-31 10:01:37 +0000

    spin up server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const cors = require("cors");
 const express = require("express");
+require("dotenv").config();
 const { default: mongoose } = require("mongoose");
 const auth = require("./routers/auth");
 const task = require("./routers/task");
-const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 app.use(
@@ -21,8 +21,8 @@ app.use("/api/auth", auth);
 app.use("/api/task", task);
 
 const port = process.env.PORT || 3000;
-mongoose.connect(process.env.pro_db_connect, () => {
-  app.listen(() => {
+mongoose.connect(process.env.dev_db_connect, () => {
+  app.listen(port, () => {
     console.log("app listening on port " + port);
   });
 });
